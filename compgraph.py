3638bda1696f465af0e0005404291b2bbeda43f4
Niall Twomey <niall.twomey@bristol.ac.uk>
2019-09-12 16:48:29 +0100

    some slight changes; improved node naming

--- compgraph.py
+++ compgraph.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 import warnings
+from os.path import join, sep
+from itertools import dropwhile, islice
 
 from . import *
 
@@ -29,10 +31,17 @@ class ComputationGraph(object):
         """
         
         self.name = name
-        self.graph_root=graph_root
+        self.graph_root = graph_root
         self.default_backend = default_backend
         self.backends = dict()
         self.nodes = dict()
+    
+    def __repr__(self):
+        """
+        
+        :return:
+        """
+        return f"<{self.__class__.__name__} name={self.name}>"
     
     def add_backend(self, name, backend):
         """
@@ -44,26 +53,15 @@ class ComputationGraph(object):
         
         self.backends[name] = backend
     
-    def evaluate(self, *nodes):
+    def evaluate_all_nodes(self, force=False):
         """
         
-        :param nodes:
-        :return:
-        """
-        
-        for node in nodes:
-            node_name = node if isinstance(node, str) else node.name
-            self.nodes[node_name].evaluate()
-    
-    def evaluate_all(self, if_exists=False):
-        """
-        
-        :param if_exists:
+        :param force:
         :return:
         """
         
         for node in self.nodes.values():
-            if (not node.exists) or (node.exists and if_exists):
+            if (not node.exists) or (node.exists and force):
                 node.evaluate()
     
     def node(self, node_name, func, sources=None, kwargs=None, metadata=None, backend=None):
@@ -127,7 +125,7 @@ class ComputationGraph(object):
                 short_node_name = node_name
                 if len(node_name) > max_len:
                     short_node_name = node_name[:max_len] + '...'
-                    
+                
                 dot.node(
                     name=str(node_ids[node_name]),
                     label=[short_node_name, short_node_name.split('/')[-1]][split_keys],
@@ -265,10 +263,15 @@ class NodeWrapper(object):
         :return:
         """
         
+        try:
+            func_name = self.func.__name__
+        except AttributeError:
+            func_name = 'functools.partial'
+        
         return '<{} sources=[{}] factor={} sink={}>'.format(
             self.__class__.__name__,
             ','.join(self.sources.keys()) if self.sources else '',
-            self.func.__name__,
+            func_name,
             self.node_name
         )
     
@@ -319,7 +322,7 @@ class NodeWrapper(object):
             func=self.func,
             backend=self.backend,
         )
-
+        
         if render:
             self.graph.render_graph(
                 root=self.graph.graph_root,
@@ -330,42 +333,56 @@ class NodeWrapper(object):
         return res
 
 
-def compute_or_load_evaluation(node_name, sources, kwargs, func, backend):
+def compute_or_load_evaluation(node_name, sources, kwargs, func, backend, split_point='build'):
     """
-    
-    :param node:
+
+    :param node_name:
     :param sources:
     :param kwargs:
     :param func:
     :param backend:
+    :param split_point:
     :return:
     """
-    
     if node_name in compute_or_load_evaluation.cache:
         return compute_or_load_evaluation.cache[node_name]
-
+    
     cache_data = hasattr(backend, 'cache_data') and backend.cache_data
-
+    
+    all_dirs = node_name.split(sep)
+    dirs = list(islice(dropwhile(
+        lambda dd: dd != split_point, all_dirs
+    ), 1, None))
+    if len(dirs) < 2:
+        node_name_short = all_dirs[-1]
+    else:
+        node_name_short = f'{sep}...{sep}'.join(
+            (dirs[0], dirs[-1])
+        )
+    
     if not backend.exists(node_name=node_name):
         inputs = (kwargs or {}).copy()
         if sources and len(sources):
             for key, value in sources.items():
                 inputs[key] = value.evaluate()
-
+        
         backend.prepare(node_name=node_name)
         
-        print('Calculating {} ...'.format(node_name), end=' ')
-        data = func(**inputs)
+        print('Calculating {} ...'.format(node_name_short), end=' ')
+        try:
+            data = func(**inputs)
+        except Exception as ex:
+            raise ex
         print('saving ...', end=' ')
         if data is not None:
             backend.save_data(node_name=node_name, data=data)
         print('done.')
     
     else:
-        print('Acquiring {}'.format(node_name), end=' ')
+        # print('Acquiring {}'.format(node_name_short), end=' ')
         data = backend.load_data(node_name=node_name)
-        print('done.')
-
+        # print('done.')
+    
     if cache_data:
         compute_or_load_evaluation.cache[node_name] = data
     
